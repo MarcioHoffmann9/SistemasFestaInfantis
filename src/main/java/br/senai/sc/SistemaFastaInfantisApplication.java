9ed9a14dc0081067b82f527aa3121c8668e471ed
Marcio <marciohoffmann9@gmail.com>
2019-04-22 20:52:58 -0300

    Projeto Criado

---
+++ src/main/java/br/senai/sc/SistemaFastaInfantisApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFastaInfantisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFastaInfantisApplication.class, args);
+	}
+
+}
